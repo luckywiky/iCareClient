2d864567ae8fe0c56d89c390a4ab8efc976bca27
luckywiky <luckywiky@gmail.com>
2013-03-27 17:53:54 +0800

    11

--- iCareClient/MainPage.xaml.cs
+++ iCareClient/MainPage.xaml.cs
@@ -4,6 +4,7 @@ using System.IO;
 using System.Linq;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
+using Windows.Storage;
 using Windows.UI.Popups;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
@@ -23,19 +24,26 @@ namespace iCareClient
     public sealed partial class MainPage : Page
     {
         private bool firstLoad = true;
+ 
+   
         public MainPage()
         {
-            this.InitializeComponent();
+            this.InitializeComponent();      
+           
         }
+
+       
+ 
+       
 
         /// <summary>
         /// 在此页将要在 Frame 中显示时进行调用。
         /// </summary>
         /// <param name="e">描述如何访问此页的事件数据。Parameter
         /// 属性通常用于配置页。</param>
-        protected override void OnNavigatedTo(NavigationEventArgs e)
+        protected   override void OnNavigatedTo(NavigationEventArgs e)
         {
-
+            
         }
 
         private  void webView_LoadCompleted(object sender, NavigationEventArgs e)
@@ -44,8 +52,10 @@ namespace iCareClient
             {
                 //async
                  // await new MessageDialog(firstLoad.ToString()).ShowAsync();
-                firstLoad = false;
+                firstLoad = false;             
             }
         }
+
+       
     }
 }
